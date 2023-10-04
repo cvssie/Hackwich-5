b9ba11fcbf5de04b501435ce0786f4ded2b48acc
Cassandra K <ckk30@hawaii.edu>
2023-10-03 22:29:51 -1000

    finished the comments

--- Hackwich 5/ViewController.swift
+++ Hackwich 5/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //comment 1 create IBOutlet for top label
+    @IBOutlet weak var topLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+       
+        topLabel.text = "My Favorite Foods"
     }
-
-
+    
+    @IBOutlet weak var bottomLabel: UILabel!
+    
+   
+    @IBAction func buttonPressed(_ sender: Any) {
+        //comment 1 create bottom label text to first index item
+        bottomLabel.text = (favoriteFoodsArray[0])
+        
+        //comment 2 create if statement for count
+        if(currentIndex > favoriteFoodsArray.count)
+        {
+            //comment 3 set next button to normal
+            buttonLabel.setTitle("Next",for: UIControl.State.normal)
+            
+            //comment 4 raise index by 1
+            currentIndex+=1
+        }
+        //comment 5 create else statement for button has been disabled
+        else
+        { print("button has been disabled")
+            (buttonLabel!).isEnabled=false
+        }
+    }
+       
+    var currentIndex = 0
+   
+    var favoriteFoodsArray: [String] = ["Sushi", "Pasta", "Ice Cream", "Pork Squash", "Lau Lau"]
+    
+    
+    
+    
+    @IBOutlet weak var buttonLabel: UIButton!
 }
 
--- Hackwich 5/secondViewController.swift
+++ Hackwich 5/secondViewController.swift
@@ -8,22 +8,70 @@
 import UIKit
 
 class secondViewController: UIViewController {
-
+    
+    @IBOutlet weak var topLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        topLabel.text = "My Favorite Food Shows"
     }
     
-
+    
     /*
-    // MARK: - Navigation
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    @IBOutlet weak var bottomLabel: UILabel!
+    
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+       //comment 1 create text of 3rd index or bottom label
+        bottomLabel.text = (favoriteShows[3])
+       
+       
+       //comment 2 create if statement to iterate through array
+        if (currentIndex > favoriteShows.count )
+            {
+                
+                
+                //comment 3 set the state of the button to normal
+                buttonLabel.setTitle("Next",for: UIControl.State.normal)
+             
+            //comment 4 increase currentindex by 1
+            currentIndex+=1
+            }
+          //comment 5 create else statement
+            else
+            {
+                print("button has been disabled")
+                (buttonLabel!).isEnabled=false
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+            }
+            
+        
+        
     }
-    */
-
+    
+    @IBOutlet weak var buttonLabel: UIButton!
+    
+       
+    
+        
+    
+    var currentIndex = 3
+    var favoriteShows: [String] = ["New Girl", "The Neighborhood", "Friends", "Gossip Girl", "One Tree Hill"]
+    
+    
+    
+   
+        
+    
 }
